5b5ac42e701fe2348d4331af27f572a06a47d349
Yuvraj Cheema <yuvrajscheema@gmail.com>
2026-04-29 01:26:44 -0700

    updated treesiter for neovim 0.12, delete rm -rf ~/.local/share/nvim/tree-sitter-* rm -rf ~/.local/share/nvim/site/parser rm -rf ~/.local/share/nvim/lazy/nvim-treesitter to make sure new parsers are installed so config works

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,66 +1,51 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
   },
   config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+    local treesitter = require("nvim-treesitter")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "java",
-        "typescript",
-        "javascript",
-        "tsx",
-        "yaml",
-        "typst",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-        "cpp",
-        "cmake",
-        "make",
-        "latex",
-        "ninja",
-        "rst",
-        "rust",
-        "python",
-        "asm",
-        "zig",
-        "vhdl"
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
+    local parsers = {
+      "json",
+      "java",
+      "typescript",
+      "javascript",
+      "tsx",
+      "yaml",
+      "typst",
+      "html",
+      "css",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "lua",
+      "vim",
+      "dockerfile",
+      "gitignore",
+      "query",
+      "vimdoc",
+      "c",
+      "cpp",
+      "cmake",
+      "make",
+      "latex",
+      "rst",
+      "rust",
+      "python",
+      "asm",
+      "zig",
+      "vhdl"
+    }
+    treesitter.install(parsers)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = parsers,
+      callback = function()
+        vim.treesitter.start()
+      end,
     })
   end,
 }
